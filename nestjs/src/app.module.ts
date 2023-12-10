c441923a0f59012f2264aef59e48d60cb1fd9a7a
Wilson Franca <wilson.franca.92@gmail.com>
2023-12-10 14:49:14 -0800

    feat(nestjs/wallets): created services and controllers for wallets and wallet assets

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AssetsModule } from './assets/assets.module';
+import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [PrismaModule, AssetsModule],
+  imports: [PrismaModule, AssetsModule, WalletsModule],
   controllers: [AppController],
   providers: [AppService],
 })
